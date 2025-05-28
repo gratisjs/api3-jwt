74dff1fa60f5c1e64052375786eb5bae87837309
gratisjs <gaaa6870@gmail.com>
2025-05-28 12:06:17 -0500

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise'); // Usamos la versión de promesas de mysql2
 
 const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+    host: process.env.DB_HOST,           // Variable de entorno de Render
+    user: process.env.DB_USER,          // Variable de entorno de Render
+    password: process.env.DB_PASSWORD,  // Variable de entorno de Render
+    database: process.env.DB_DATABASE,    // Variable de entorno de Render
+    port: process.env.DB_PORT          // Variable de entorno de Render
 };
 
 // Crear un pool de conexiones para manejar múltiples conexiones de forma eficiente
