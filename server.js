2c929755a721650e09e324fb0c921b14ec4cdbda
gratisjs <gaaa6870@gmail.com>
2025-05-28 12:07:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config(); // Carga las variables de entorno desde .env
 const app = require('./src/app'); // Importa la configuración de la app desde src/app.js
 
-const PORT = process.env.PORT || 5000; // Usa el puerto del .env o 5000 por defecto
+const PORT = process.env.PORT || 5009; // Usa el puerto del .env o 5000 por defecto
 
 // Iniciar el servidor
 app.listen(PORT, () => {
